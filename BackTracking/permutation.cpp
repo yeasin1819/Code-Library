ddaf733779e31827995f4212a45284154fc39e90
mdarafat1819 <52718417+mdarafat1819@users.noreply.github.com>
2020-04-26 21:52:15 +0600

    Create permutation.cpp

---
+++ BackTracking/permutation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void permutation(int ara[], int start, int end)
+{
+    if(start == (end))
+    {
+        for(int i = 0; i <= end; i++)
+            cout<<ara[i]<<" ";
+        cout<<endl;
+        return;
+    }
+    for(int i = start; i <= end; i++)
+    {
+        swap(ara[start], ara[i]);
+        permutation(ara, start + 1, end);
+        swap(ara[start], ara[i]);
+    }
+}
+
+int main()
+{
+    int ara[] = {1, 2, 3};
+    permutation(ara, 0, 2);
+    return 0;
+}
