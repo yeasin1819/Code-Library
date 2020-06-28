c8ef8fecb11f852eb805923b4fcd07d616d03254
mdarafat1819 <52718417+mdarafat1819@users.noreply.github.com>
2020-06-28 19:41:26 +0600

    Add files via upload

---
+++ Data Structure/Linked List/4. delete a node at spacific position.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef struct node Node;
+
+struct node
+{
+    int data;
+    Node *next;
+};
+
+void append(Node *head, int data)
+{
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    new_node -> data = data;
+    new_node -> next = NULL;
+    while(head -> next != NULL) head = head -> next;
+    head -> next = new_node;
+}
+
+void prepend(Node *head, int data)
+{
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    new_node -> data = data;
+    new_node -> next = head -> next;
+    head -> next = new_node;
+}
+
+void insert_node_nth_pos(Node *head, int pos, int data)
+{
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    new_node -> data = data;
+    new_node -> next = NULL;
+    while((head -> next != NULL) && pos)
+    {
+        head = head -> next;
+        pos--;
+    }
+    new_node -> next = head -> next;
+    head -> next = new_node;
+}
+
+void delete_node_nth_pos(Node *head, int pos)
+{
+    while((head -> next != NULL) && (pos > 1)) head = head -> next, pos--;
+    head -> next = head -> next -> next;
+}
+
+void print(Node *head)
+{
+    while(head -> next != NULL)
+    {
+        cout<<head -> next -> data<<" ";
+        head = head -> next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Node *head = (Node*)malloc(sizeof(Node));
+    head -> next = NULL;
+
+    append(head, 1);
+    append(head, 2);
+    prepend(head, 4);
+    print(head);
+    insert_node_nth_pos(head, 1, 10);
+    print(head);
+    delete_node_nth_pos(head, 4);
+    print(head);
+
+    return 0;
+}
