a33217464c98aa82c45bb11e90a08b96dd2db8f0
mdarafat1819 <52718417+mdarafat1819@users.noreply.github.com>
2020-06-28 11:39:58 +0600

    Create prepend a node in linked list.cpp

---
+++ Data Structure/Linked List/prepend a node in linked list.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef struct node Node;
+
+struct node
+{
+    int data;
+    Node *next;
+};
+
+void append(Node *head, int data)
+{
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    new_node -> data = data;
+    new_node -> next = NULL;
+    while(head -> next != NULL) head = head -> next;
+    head -> next = new_node;
+}
+
+void prepend(Node *head, int data)
+{
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    new_node -> data = data;
+    new_node -> next = head -> next;
+    head -> next = new_node;
+}
+
+void print(Node *head)
+{
+    while(head -> next != NULL)
+    {
+        cout<<head -> next -> data<<" ";
+        head = head -> next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Node *head = (Node*)malloc(sizeof(Node));
+    head -> next = NULL;
+
+    append(head, 1);
+    prepend(head, 5);
+    append(head, 2);
+    print(head);
+    append(head, 3);
+    prepend(head, 4);
+    print(head);
+
+    return 0;
+}
