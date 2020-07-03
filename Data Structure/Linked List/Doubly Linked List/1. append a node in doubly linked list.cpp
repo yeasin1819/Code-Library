24dc3d4d6051186730da70031ebd3443029c104d
mdarafat1819 <mdarafat1819@gmail.com>
2020-07-03 17:37:20 +0600

    Add files via upload

---
+++ Data Structure/Linked List/Doubly Linked List/1. append a node in doubly linked list.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef struct node Node;
+
+struct node
+{
+    int data;
+    Node *prev;
+    Node *next;
+};
+
+void append(Node *head, int data)
+{
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    new_node -> data = data;
+    new_node -> prev = NULL;
+    new_node -> next = NULL;
+    while(head -> next != NULL) head = head -> next;
+    new_node -> prev = head;
+    head -> next = new_node;
+}
+
+void print(Node *head)
+{
+    while(head -> next != NULL)
+    {
+        cout<<head -> next -> data<<" ";
+        head = head -> next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Node *head = (Node*)malloc(sizeof(Node));
+    head -> prev = NULL;
+    head -> next = NULL;
+    append(head, 1);
+    append(head, 2);
+    print(head);
+    append(head, 3);
+    print(head);
+    return 0;
+}
