41554a74f012b92e430e0c9cd32e408e74e76b9c
mdarafat1819 <mdarafat1819@gmail.com>
2020-07-05 23:41:33 +0600

    Update 6. reverse a doubly linked list.cpp

--- Data Structure/Linked List/Doubly Linked List/6. reverse a doubly linked list.cpp
+++ Data Structure/Linked List/Doubly Linked List/6. reverse a doubly linked list.cpp
@@ -23,12 +23,11 @@ void append(Node *head, int data)
 }
 void reverse_print(Node *head)
 {
-    Node *temp = head -> next;
-    while(temp -> next != NULL) temp = temp -> next;
-    while(temp -> prev != NULL)
+    while(head -> next != NULL) head = head -> next;
+    while(head -> prev != NULL)
     {
-        cout<<temp -> data<<" ";
-        temp = temp -> prev;
+        cout<<head -> data<<" ";
+        head = head -> prev;
     }
     cout<<endl;
 }
